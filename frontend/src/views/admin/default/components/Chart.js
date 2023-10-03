bdb4db6a417c3d7430f36dcdd5dff8dbb7be81e8
Andrew Lee <andrewlee@andrews-air-2.home>
2023-10-03 12:18:46 -0400

    Fixed graph time

--- frontend/src/views/admin/default/components/Chart.js
+++ frontend/src/views/admin/default/components/Chart.js
@@ -16,138 +16,138 @@ import { MdBarChart, MdOutlineCalendarToday } from "react-icons/md";
 // Assets
 import { RiArrowUpSFill } from "react-icons/ri";
 
-
 const lineChartDataTotalSpent = [
   {
     name: "Revenue",
     data: [55000, 64, 48, 66, 49, 68, 50],
-  }
+  },
 ];
 
 const testOptions = {
   chart: {
-    type: 'area',
+    type: "area",
     stacked: false,
     zoom: {
-      type: 'x',
+      type: "x",
       enabled: false,
-      autoScaleYaxis: true
+      autoScaleYaxis: true,
     },
     toolbar: {
-      autoSelected: 'zoom'
-    }
+      autoSelected: "zoom",
+    },
   },
   dataLabels: {
-    enabled: false
+    enabled: false,
   },
 
   annotations: {
-
-    yaxis: [{
-      y: 30,
-      borderColor: '#999',
-      label: {
-        show: true,
-        text: 'Support',
-        style: {
-          color: "#fff",
-          background: '#00E396'
-        }
-      }
-    }],
-    xaxis: [{
-      x: new Date('14 Nov 2012').getTime(),
-      borderColor: '#999',
-      yAxisIndex: 0,
-      label: {
-        show: true,
-        text: 'Rally',
-        style: {
-          color: "#fff",
-          background: '#8D5DD0'
-        }
-      }
-    }]
+    yaxis: [
+      {
+        y: 30,
+        borderColor: "#999",
+        label: {
+          show: true,
+          text: "Support",
+          style: {
+            color: "#fff",
+            background: "#00E396",
+          },
+        },
+      },
+    ],
+    xaxis: [
+      {
+        x: new Date("14 Nov 2012").getTime(),
+        borderColor: "#999",
+        yAxisIndex: 0,
+        label: {
+          show: true,
+          text: "Rally",
+          style: {
+            color: "#fff",
+            background: "#8D5DD0",
+          },
+        },
+      },
+    ],
   },
   markers: {
     size: 0,
-    style: 'hollow',
-    colors: '#AAAAAA'
+    style: "hollow",
+    colors: "#AAAAAA",
   },
   title: {
-    text: '',
-    align: 'left'
+    text: "",
+    align: "left",
   },
   fill: {
-    colors: ['#FFAF00'],
-    type: 'gradient',
+    colors: ["#FFAF00"],
+    type: "gradient",
     gradient: {
       shadeIntensity: 1,
       opacityFrom: 0.7,
       opacityTo: 0.9,
-      stops: [0, 100]
-    }
+      stops: [0, 100],
+    },
   },
   stroke: {
     curve: "straight",
     type: "line",
-    colors: ["#FFAF00"]
+    colors: ["#FFAF00"],
   },
   yaxis: {
     labels: {
       formatter: function (val) {
-        return val.toFixed(0)
+        return val.toFixed(0);
         // (val / 1000000).toFixed(0);
       },
     },
     title: {
-      text: ''
+      text: "",
     },
   },
   xaxis: {
-    type: 'datetime',
+    type: "datetime",
     title: {
-      text: ''
+      text: "",
     },
     labels: {
       formatter: function (val) {
-        const date = new Date(val * 1000); // Convert epoch time to milliseconds
+        const date = new Date(val); // Convert epoch time to milliseconds
         const day = date.getDate();
-        const month = date.toLocaleString('en-us', { month: 'short' }); // Get short month name (e.g., "Sep")
+        const month = date.toLocaleString("en-us", { month: "short" }); // Get short month name (e.g., "Sep")
 
         return `${day} ${month}`;
-      }
+      },
     },
-    tickAmount: 6
+    tickAmount: 6,
   },
   tooltip: {
     fillSeriesColor: false,
     marker: {
-      show: false
+      show: false,
     },
 
     x: {
       show: true,
-      format: 'dd MMM yyyy'
+      format: "dd MMM yyyy",
     },
     shared: false,
     y: {
       title: {
         formatter: function (val) {
-          return
-        }
+          return;
+        },
       },
       formatter: function (val) {
-        return '$' + val.toFixed(2)
+        return "$" + val.toFixed(2);
         //  (val / 1000000).toFixed(0)
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 
 export default function TotalSpent({ coinData, graphData, ...rest }) {
- 
-  
   const textColor = useColorModeValue("secondaryGray.900", "white");
   const textColorSecondary = useColorModeValue("secondaryGray.600", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
@@ -161,91 +161,91 @@ export default function TotalSpent({ coinData, graphData, ...rest }) {
     { bg: "secondaryGray.300" },
     { bg: "whiteAlpha.100" }
   );
-  return graphData ?
-    (
-      <Card
-        justifyContent='center'
-        align='center'
-        direction='column'
-        w='100%'
-        mb='0px'
-        {...rest}>
-        <Flex justify='space-between' ps='0px' pe='20px' pt='5px'>
-          <Flex align='center' w='100%'>
-            <Button
-              bg={boxBg}
-              fontSize='sm'
-              fontWeight='500'
+  return graphData ? (
+    <Card
+      justifyContent="center"
+      align="center"
+      direction="column"
+      w="100%"
+      mb="0px"
+      {...rest}
+    >
+      <Flex justify="space-between" ps="0px" pe="20px" pt="5px">
+        <Flex align="center" w="100%">
+          <Button
+            bg={boxBg}
+            fontSize="sm"
+            fontWeight="500"
+            color={textColorSecondary}
+            borderRadius="7px"
+          >
+            <Icon
+              as={MdOutlineCalendarToday}
               color={textColorSecondary}
-              borderRadius='7px'>
-              <Icon
-                as={MdOutlineCalendarToday}
-                color={textColorSecondary}
-                me='4px'
-              />
-              This month
-            </Button>
-            <Button
-              ms='auto'
-              align='center'
-              justifyContent='center'
-              bg={bgButton}
-              _hover={bgHover}
-              _focus={bgFocus}
-              _active={bgFocus}
-              w='37px'
-              h='37px'
-              lineHeight='100%'
-              borderRadius='10px'
-              {...rest}
-              >
-              <Icon as={MdBarChart} color={iconColor} w='24px' h='24px' />
-            </Button>
-          </Flex>
+              me="4px"
+            />
+            This month
+          </Button>
+          <Button
+            ms="auto"
+            align="center"
+            justifyContent="center"
+            bg={bgButton}
+            _hover={bgHover}
+            _focus={bgFocus}
+            _active={bgFocus}
+            w="37px"
+            h="37px"
+            lineHeight="100%"
+            borderRadius="10px"
+            {...rest}
+          >
+            <Icon as={MdBarChart} color={iconColor} w="24px" h="24px" />
+          </Button>
         </Flex>
-        <Flex w='100%' flexDirection={{ base: "column", lg: "row" }}>
-          <Flex flexDirection='column' me='20px' mt='28px'>
+      </Flex>
+      <Flex w="100%" flexDirection={{ base: "column", lg: "row" }}>
+        <Flex flexDirection="column" me="20px" mt="28px">
+          <Text
+            color={textColor}
+            fontSize="34px"
+            textAlign="start"
+            fontWeight="700"
+            lineHeight="100%"
+          >
+            {coinData.market_data.current_price["usd"]}
+          </Text>
+          <Flex align="center" mb="20px">
             <Text
-              color={textColor}
-              fontSize='34px'
-              textAlign='start'
-              fontWeight='700'
-              lineHeight='100%'>
-              {coinData.market_data.current_price["usd"]}
+              color="secondaryGray.600"
+              fontSize="sm"
+              fontWeight="500"
+              mt="4px"
+              me="12px"
+            >
+              Total Spent
             </Text>
-            <Flex align='center' mb='20px'>
-              <Text
-                color='secondaryGray.600'
-                fontSize='sm'
-                fontWeight='500'
-                mt='4px'
-                me='12px'>
-                Total Spent
-              </Text>
-              <Flex align='center'>
-                <Icon as={RiArrowUpSFill} color='green.500' me='2px' mt='2px' />
-                <Text color='green.500' fontSize='sm' fontWeight='700'>
-                  +2.45%
-                </Text>
-              </Flex>
-            </Flex>
-  
-            <Flex align='center'>
-              <Icon as={IoCheckmarkCircle} color='green.500' me='4px' />
-              <Text color='green.500' fontSize='md' fontWeight='700'>
-                On track
+            <Flex align="center">
+              <Icon as={RiArrowUpSFill} color="green.500" me="2px" mt="2px" />
+              <Text color="green.500" fontSize="sm" fontWeight="700">
+                +2.45%
               </Text>
             </Flex>
           </Flex>
-          <Box minH='260px' minW='75%' mt='auto'>
-            <LineChart
-            chartData={graphData}
-            chartOptions={testOptions}
-          />
-          </Box>
+
+          <Flex align="center">
+            <Icon as={IoCheckmarkCircle} color="green.500" me="4px" />
+            <Text color="green.500" fontSize="md" fontWeight="700">
+              On track
+            </Text>
+          </Flex>
         </Flex>
-      </Card>
-    )
-    :
-    (<></>)
+        <Box minH="260px" minW="75%" mt="auto">
+          <LineChart chartData={graphData} chartOptions={testOptions} />
+        </Box>
+      </Flex>
+    </Card>
+  ) : (
+    <></>
+  );
 }

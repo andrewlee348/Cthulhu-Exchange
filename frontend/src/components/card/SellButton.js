3f4a76030e823772557a4280307d6a06ea2c033b
Andrew Lee <andrewlee@andrews-air-2.home>
2023-10-21 13:02:43 -0400

    minor sell button changes

--- frontend/src/components/card/SellButton.js
+++ frontend/src/components/card/SellButton.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import { Button, Flex } from '@chakra-ui/react';
-import ActionButton from './ActionButton';
-import { FaDollarSign } from 'react-icons/fa';
+import React from "react";
+import { Button, Flex } from "@chakra-ui/react";
+import ActionButton from "./ActionButton";
+import { FaDollarSign } from "react-icons/fa";
 const SellButton = () => {
-    const handleSell = () => {
-        // handle sell logic here
-    };
+  const handleSell = () => {
+    // handle sell logic here
+  };
 
-    return (
-        <ActionButton title="Sell" icon={<FaDollarSign color="white" />}>
-            <Flex direction="column">
-                skrrrat
-                <Button
-                    variant="darkBrand"
-                    color="white"
-                    fontSize="sm"
-                    fontWeight="500"
-                    borderRadius="70px"
-                    px="16px"
-                    py="5px"
-                    ml="10px"
-                    onClick={handleSell}>
-                    Sell
-                </Button>
-            </Flex>
-        </ActionButton>
-    );
+  return (
+    <ActionButton title="Sell" icon={<FaDollarSign color="white" />}>
+      <Flex direction="column">
+        skrrrat
+        <Button
+          variant="darkBrand"
+          color="white"
+          fontSize="sm"
+          fontWeight="500"
+          borderRadius="70px"
+          px="16px"
+          py="5px"
+          ml="10px"
+          onClick={handleSell}
+        >
+          Sell
+        </Button>
+      </Flex>
+    </ActionButton>
+  );
 };
 
 export default SellButton;
